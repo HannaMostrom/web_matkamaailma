545a6a1cc833f9dca429059a4c1eb6b3fbef5d52
HannaMostrom <hannamostrom91@gmail.com>
2024-02-29 19:35:10 +0200

    WIP7

--- my-app/src/components/ContactForm.js
+++ my-app/src/components/ContactForm.js
@@ -11,41 +11,49 @@ function ContactForm() {
           <label className="label-kolmonen" for="fname">
             Nimi
           </label>
+          <br></br>
           <input
             type="text"
             name="nimi"
             placeholder="Nimi"
             className="tekstit-kaksi"
           />
+          <br></br>
         </form>
         <form>
           <label className="label-kolmonen" for="number">
             Puhelinnumero
           </label>
+          <br></br>
           <input
             type="text"
             name="puhelinnumero"
             placeholder="Puhelinnumero"
             className="tekstit-kaksi"
           />
+          <br></br>
           <label className="label-kolmonen" for="email">
             Sähköpostiosoite
           </label>
+          <br></br>
           <input
             type="text"
             name="sahköpostiosoite"
             placeholder="Sähköpostiosoite"
             className="tekstit-kaksi"
           />
+          <br></br>
           <label className="label-kolmonen" for="info">
             Lisätiedot
           </label>
+          <br></br>
           <input
             type="text2"
             name="lisatiedot"
             placeholder=""
             className="tekstit-kaksi"
           />
+          <br></br>
           <input type="submit" value="Lähetä"></input>
         </form>
       </div>
