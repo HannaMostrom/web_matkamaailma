77a374e8cb163eb298c90b7f2f793c8f8caeae2c
HannaMostrom <hannamostrom91@gmail.com>
2024-03-03 10:17:30 +0200

    WIP9

--- my-app/src/components/ContactForm.js
+++ my-app/src/components/ContactForm.js
@@ -2,60 +2,72 @@ import { React } from "react";
 
 import "../App.css";
 
+import { Container, Row, Col } from "react-bootstrap";
+
 function ContactForm() {
   return (
     <div>
-      <h3 className="neljasOtsikko">YHTEYDENOTTOLOMAKE</h3>
-      <div>
-        <form>
-          <label className="label-kolmonen" for="fname">
-            Nimi
-          </label>
-          <br></br>
-          <input
-            type="text"
-            name="nimi"
-            placeholder="Nimi"
-            className="tekstit-kaksi"
-          />
-          <br></br>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h3 className="neljasOtsikko">YHTEYDENOTTOLOMAKE</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <form>
+                <label className="label-kolmonen" for="fname">
+                  Nimi
+                </label>
+                <br></br>
+                <input
+                  type="text"
+                  name="nimi"
+                  placeholder="Nimi"
+                  className="tekstit-kaksi"
+                />
+                <br></br>
 
-          <label className="label-kolmonen" for="number">
-            Puhelinnumero
-          </label>
-          <br></br>
-          <input
-            type="text"
-            name="puhelinnumero"
-            placeholder="Puhelinnumero"
-            className="tekstit-kaksi"
-          />
-          <br></br>
-          <label className="label-kolmonen" for="email">
-            Sähköpostiosoite
-          </label>
-          <br></br>
-          <input
-            type="text"
-            name="sahköpostiosoite"
-            placeholder="Sähköpostiosoite"
-            className="tekstit-kaksi"
-          />
-          <br></br>
-          <label className="label-kolmonen" for="info">
-            Lisätiedot
-          </label>
-          <br></br>
-          <input
-            type="text2"
-            name="lisatiedot"
-            placeholder=""
-            className="tekstit-kaksi"
-          />
-          <br></br>
-          <input type="submit" value="Lähetä"></input>
-        </form>
-      </div>
+                <label className="label-kolmonen" for="number">
+                  Puhelinnumero
+                </label>
+                <br></br>
+                <input
+                  type="text"
+                  name="puhelinnumero"
+                  placeholder="Puhelinnumero"
+                  className="tekstit-kaksi"
+                />
+                <br></br>
+                <label className="label-kolmonen" for="email">
+                  Sähköpostiosoite
+                </label>
+                <br></br>
+                <input
+                  type="text"
+                  name="sahköpostiosoite"
+                  placeholder="Sähköpostiosoite"
+                  className="tekstit-kaksi"
+                />
+                <br></br>
+                <label className="label-kolmonen" for="info">
+                  Lisätiedot
+                </label>
+                <br></br>
+                <input
+                  type="text2"
+                  name="lisatiedot"
+                  placeholder=""
+                  className="tekstit-kaksi"
+                />
+                <br></br>
+                <input type="submit" value="Lähetä"></input>
+              </form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
